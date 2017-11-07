8fb3c7282dca12bf8eee98408730e66d2d16117d
lildavey <david@perrens.com>
2017-11-07 13:25:57 -0600

    xc fd cfdvfvc

---
+++ src/Runner.java
@@ -0,0 +1,31 @@
+
+import mayflower.Mayflower;
+import mayflower.World;
+
+public class Runner extends Mayflower{
+
+    private World world;
+    public Runner()
+    {
+        super("Snake",800,600);
+
+
+    }
+    public void init()
+    {
+       setWorld(new MyWorld());
+        showBounds(true);
+
+    }
+    public static void main(String args[])
+    {
+        new Runner();
+        //setWorld(new MyWorld());
+    }
+//    public void update(org.newdawn.slick.GameContainer arg0, int arg1)
+//    {
+//        //setWorld(new MyWorld());
+//
+//    }
+
+}
