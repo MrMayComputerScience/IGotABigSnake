c8935c86fc8210cba88d8cd26d6abad757b968b6
lildavey <david@perrens.com>
2018-01-19 13:57:09 -0600

    action

--- src/Action.java
+++ src/Action.java
@@ -1,3 +1,19 @@
 public class Action {
-    
+
+    private int player;
+    private String action;
+
+    public Action(int player, String action)
+    {
+        this.player=player;
+        this.action=action;
+    }
+
+    public int getPlayer() {
+        return player;
+    }
+
+    public String getAction() {
+        return action;
+    }
 }
