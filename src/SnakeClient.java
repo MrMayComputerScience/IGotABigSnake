f70a8443710a2dc10e9eb82c4d56956fb0c4660c
lildavey <david@perrens.com>
2018-01-25 13:54:16 -0600

    snakeclient

---
+++ src/SnakeClient.java
@@ -0,0 +1,18 @@
+import mayflower.net.Client;
+
+public class SnakeClient extends Client {
+    @Override
+    public void process(String s) {
+        
+    }
+
+    @Override
+    public void onDisconnect(String s) {
+
+    }
+
+    @Override
+    public void onConnect() {
+
+    }
+}
